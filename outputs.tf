7152a9d4ef988e14ff49e255a7e554da4a433ce2
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "private_endpoint_application_security_group_associations" {
   description = "All private_endpoint_application_security_group_association resources"
   value       = azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations
 }
+output "private_endpoint_application_security_group_associations_application_security_group_id" {
+  description = "List of application_security_group_id values across all private_endpoint_application_security_group_associations"
+  value       = [for k, v in azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations : v.application_security_group_id]
+}
+output "private_endpoint_application_security_group_associations_private_endpoint_id" {
+  description = "List of private_endpoint_id values across all private_endpoint_application_security_group_associations"
+  value       = [for k, v in azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations : v.private_endpoint_id]
+}
 
