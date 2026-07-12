69339a7230b90e6ae828501c586470cf7fc243b3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "private_endpoint_application_security_group_associations_id" {
+  description = "Map of id values across all private_endpoint_application_security_group_associations, keyed the same as var.private_endpoint_application_security_group_associations"
+  value       = { for k, v in azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations : k => v.id }
+}
 output "private_endpoint_application_security_group_associations_application_security_group_id" {
   description = "Map of application_security_group_id values across all private_endpoint_application_security_group_associations, keyed the same as var.private_endpoint_application_security_group_associations"
   value       = { for k, v in azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations : k => v.application_security_group_id }
