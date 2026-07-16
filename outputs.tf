15f1b87e251f81be80c820870b526149696a94bc
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "private_endpoint_application_security_group_associations_id" {
   description = "Map of id values across all private_endpoint_application_security_group_associations, keyed the same as var.private_endpoint_application_security_group_associations"
-  value       = { for k, v in azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations : k => v.id }
+  value       = { for k, v in azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "private_endpoint_application_security_group_associations_application_security_group_id" {
   description = "Map of application_security_group_id values across all private_endpoint_application_security_group_associations, keyed the same as var.private_endpoint_application_security_group_associations"
-  value       = { for k, v in azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations : k => v.application_security_group_id }
+  value       = { for k, v in azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations : k => v.application_security_group_id if v.application_security_group_id != null && length(v.application_security_group_id) > 0 }
 }
 output "private_endpoint_application_security_group_associations_private_endpoint_id" {
   description = "Map of private_endpoint_id values across all private_endpoint_application_security_group_associations, keyed the same as var.private_endpoint_application_security_group_associations"
-  value       = { for k, v in azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations : k => v.private_endpoint_id }
+  value       = { for k, v in azurerm_private_endpoint_application_security_group_association.private_endpoint_application_security_group_associations : k => v.private_endpoint_id if v.private_endpoint_id != null && length(v.private_endpoint_id) > 0 }
 }
 
